dec66dd7f6473a0f0d2e8b0b2c12de2bb8cb68a8
dryga94 <dryga94@gmail.com>
2020-08-19 21:33:56 +0300

    fix funcs

--- src/games/game-flow.js
+++ src/games/game-flow.js
@@ -2,6 +2,8 @@ import readlineSync from 'readline-sync';
 import isEven from '../helpers/is-even.js';
 import getRandomNum from '../helpers/get-random-number.js';
 
+export const getOperand = () => getRandomNum(100);
+
 export const isAnswerCorrect = (answ, value) => answ === value;
 
 export const getCorrectAnswer = (value) => (isEven(value) ? 'yes' : 'no');
@@ -10,7 +12,7 @@ const gameFlow = () => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   let counter = 0;
   for (let i = 0; i < 3; i += 1) {
-    const num = getRandomNum(100);
+    const num = getOperand();
     const answer = readlineSync.question(`Question: ${num}\n`);
     if (!isAnswerCorrect(answer, getCorrectAnswer(num))) {
       console.log(`"${answer}" is incorrect, correct answer is "${getCorrectAnswer(num)}"`);
