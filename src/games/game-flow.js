5fb31575cb18049f72a816dd935dfe39040ec23c
dryga94 <dryga94@gmail.com>
2020-08-19 22:06:51 +0300

    update brain even

--- src/games/game-flow.js
+++ src/games/game-flow.js
@@ -8,20 +8,30 @@ export const isAnswerCorrect = (answ, value) => answ === value;
 
 export const getCorrectAnswer = (value) => (isEven(value) ? 'yes' : 'no');
 
+export const defineTheRule = () => console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+export const formQuestions = () => {
+  const num = getOperand();
+  const answer = readlineSync.question(`Question: ${num}\n`);
+  return { num, answer };
+};
+
+export const checkIteration = (iter) => {
+  if (!isAnswerCorrect(iter.answer, getCorrectAnswer(iter.num))) {
+    console.log(`"${iter.answer}" is incorrect, correct answer is "${getCorrectAnswer(iter.num)}"`);
+    return false;
+  }
+  console.log('Correct');
+  return true;
+};
+
 const gameFlow = () => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  defineTheRule();
   let counter = 0;
   for (let i = 0; i < 3; i += 1) {
-    const num = getOperand();
-    const answer = readlineSync.question(`Question: ${num}\n`);
-    if (!isAnswerCorrect(answer, getCorrectAnswer(num))) {
-      console.log(`"${answer}" is incorrect, correct answer is "${getCorrectAnswer(num)}"`);
-      return false;
-    }
-    console.log('Correct');
-    counter += 1;
-    if (counter === 3) return true;
+    if (checkIteration(formQuestions())) counter += 1;
+    else break;
   }
-  return true;
+  return counter === 3;
 };
 export default gameFlow;
